6daad2179a8788cbd7f843a3b0bd0cc47b159e86
Heeyun Lee <lhy7023@gmail.com>
2021-09-29 23:18:27 -0400

    version 0.3.6+12 get rid of pods folder

--- ios/Pods (1)/Target Support Files/kakao_flutter_sdk/kakao_flutter_sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_kakao_flutter_sdk : NSObject
-@end
-@implementation PodsDummy_kakao_flutter_sdk
-@end
--- ios/Pods (1)/Target Support Files/mixpanel_flutter/mixpanel_flutter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mixpanel_flutter : NSObject
-@end
-@implementation PodsDummy_mixpanel_flutter
-@end
